cf4bc7b13311ae778a78acfb7dc26f7e1b768693
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-18 10:51:28 -0700

    got zigzag to work

--- assets/javascript/zigzag.js
+++ assets/javascript/zigzag.js
@@ -2,7 +2,8 @@ let canvas = document.getElementById('third-canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let x = 0;
-let y = (canvas.height * 3 / 4)
+let y = (canvas.height * 3 / 4);
+let dy = 10;
 
 // function zigZag() {
 //   let ctx = canvas.getContext('2d');
@@ -19,20 +20,28 @@ let y = (canvas.height * 3 / 4)
 //   ctx.stroke();
 // }
 
-function drawPixel() {
+function drawPixel(currentAnimation) {
   let ctx = canvas.getContext('2d');
   ctx.beginPath();
-  ctx.fillRect(x, y, 10, 10)
+  ctx.arc(x, y, 5, 0, Math.PI * 2, false);
   ctx.strokeStyle = 'blue';
-  ctx.lineWidth = 8;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+  ctx.lineWidth = 5;
   ctx.stroke();
   console.log(x)
   console.log(y)
-  x += 3
-  y += 1
-  if (x === (canvas.width * 2 / 5) || y === (canvas.height / 3)) {
-    x += 5
-    y -= 5
+  x += 5
+  y += dy
+  if (y > canvas.height || y < 0) {
+    dy = -1 * dy
+  }
+  if (x > canvas.width) {
+    x = 0;
+    y = (canvas.height * 3 / 4);
+    dy = 10;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cancelAnimationFrame(currentAnimation)
   }
 }
 
@@ -41,8 +50,9 @@ function drawPixel() {
 // }
 
 export default function zigZag() {
-  window.requestAnimFrame(zigZag)
-  drawPixel();
+  let currentAnimation = requestAnimFrame(zigZag)
+  drawPixel(currentAnimation);
+
 }
 // firstPath();
 // secondPath();
